2e50cc327172e1405d674bb885b3a259ab92746c
Che Smith <che.smith.usa@gmail.com>
2023-09-11 10:33:19 -0500

    Add project files.

---
+++ CSP.ASP_NET/CaptureStreamFilter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CSP.ASP_NET
+{
+    public class CaptureStreamFilter : Stream
+    {
+        private Stream _originalStream;
+        private string _nonce;
+        private ContentLengthContainer _contentLengthContainer;
+
+        public CaptureStreamFilter(Stream originalStream, string nonce, ContentLengthContainer contentLengthContainer)
+        {
+            _originalStream = originalStream;
+            _nonce = nonce;
+            _contentLengthContainer = contentLengthContainer;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            byte[] data = new byte[count];
+            Buffer.BlockCopy(buffer, offset, data, 0, count);
+            string inputstring = Encoding.UTF8.GetString(data);
+            data = Encoding.UTF8.GetBytes(inputstring.Replace("{nonce}", _nonce));
+            _originalStream.Write(data, 0, count);
+
+            _contentLengthContainer.TotalContentLength += count;
+        }
+
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => throw new NotSupportedException();
+
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+
+        public override void Flush()
+        {
+            _originalStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class ContentLengthContainer
+    {
+        public long TotalContentLength { get; set; }
+    }
+}
---
+++ CSP.ASP_NET/CspHttpModule.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace CSP.ASP_NET
+{
+    public class CspHttpModule : IHttpModule
+    {
+        private string _nonce;
+        private ContentLengthContainer _contentLengthContainer = new ContentLengthContainer();
+
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+            context.PostReleaseRequestState += OnPostReleaseRequestState;
+
+            _nonce = GenerateNonce();
+        }
+
+        public void Dispose()
+        {
+            // Cleanup code here, if needed
+        }
+
+        private void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)sender;
+            HttpContext context = app.Context;
+
+            // Set your CSP policy with the nonce
+            string cspPolicy = $"default-src 'self' 'nonce-{_nonce}'; script-src-attr 'unsafe-inline'; style-src 'self' 'unsafe-inline'";
+
+            // Add CSP header to the response
+            context.Response.Headers.Add("Content-Security-Policy", cspPolicy);
+        }
+
+        private string GenerateNonce()
+        {
+            using (var rngCryptoServiceProvider = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                var nonceBytes = new byte[32];
+                rngCryptoServiceProvider.GetBytes(nonceBytes);
+                return Convert.ToBase64String(nonceBytes);
+            }
+        }
+
+        private void OnPostReleaseRequestState(object sender, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)sender;
+            HttpContext context = app.Context;
+
+            // Capture and modify the response content
+            context.Response.Filter = new CaptureStreamFilter(context.Response.Filter, _nonce, _contentLengthContainer);
+            //context.Response.Filter = CaptureResponseStream(context.Response, context.Response.Filter);
+            System.Diagnostics.Debug.WriteLine(_contentLengthContainer.TotalContentLength);
+            //context.Response.Headers["Content-Length"] = _contentLengthContainer.TotalContentLength.ToString();
+        }
+
+        //private Stream CaptureResponseStream(HttpResponse response, Stream originalStream)
+        //{
+        //    Stream captureStream = new MemoryStream();
+        //    //response.Filter = new CaptureStreamFilter(originalStream, captureStream, _nonce);
+        //    response.Filter = new SimpleStreamFilter(originalStream);
+        //    return captureStream;
+        //}
+    }
+}
